2f87c87f74c05e3a2487f2f9dda6da05611a01dd
tdyer <tel.dyer@canopytax.com>
2018-11-01 15:52:08 -0600

    basic functionality working

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 var messages = [];
+var curr_index = 1;
 
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
+function getFormattedTime() {
+    let date = new Date();
+    let hours = date.getHours() % 12 === 0 ? 12 : date.getHours() % 12;
+    let minutes = (date.getMinutes()<10?'0':'') + date.getMinutes();
+    return hours + ':' + minutes;
+}
+
 io.on('connection', function(client) {
-    console.log('Client connected...');
 
     client.on('join', function (data) {
-        messages.push(data);
-        console.log(messages);
+        client.emit('initial', messages);
+        client.emit('broad', {sender: 'SlackExchange', time: getFormattedTime(), content: 'Welcome to SlackExchange. Pick a name or remain anonymous and start slacking away!'})
+        client.emit('anonymous-index', {index: curr_index++})
     });
 
     client.on('messages', function(data) {
+        console.log(data.content);
         messages.push(data);
-        client.emit('messages', data);
+        io.emit('broad', data);
     });
 });
 
